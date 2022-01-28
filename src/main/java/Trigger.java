b183a530d127aaa7667442ee57a77d18bd0a7f6b
ArvikVan <arvikvan@gmail.com>
2022-01-28 21:47:35 +0300

    first commit

---
+++ src/main/java/Trigger.java
@@ -0,0 +1,10 @@
+/**
+ * @author ArvikV
+ * @version 1.0
+ * @since 28.01.2022
+ */
+public class Trigger {
+    public int someLogic() {
+        return 1;
+    }
+}
---
+++ src/test/java/TriggerTest.java
@@ -0,0 +1,16 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author ArvikV
+ * @version 1.0
+ * @since 28.01.2022
+ */
+public class TriggerTest {
+    @Test
+    public void someLogic() {
+        Assert.assertEquals(1, new Trigger().someLogic());
+    }
+}
